1170a5137e653ec5a422a477802cfa3e1c54a824
diGiovani <alexsandro.dgjr@gmail.com>
2021-09-08 10:18:10 -0300

    trying to fix logout loop

--- pages/auth/signout.js
+++ pages/auth/signout.js
@@ -1,9 +1,11 @@
 import { signOut } from 'next-auth/client'
+import { useRouter } from 'next/router'
+
 
 export default function Signout() {
   if (typeof window !== 'undefined') {
     signOut({ callbackUrl: process.env.NEXTAUTH_URL })
   }
-
+  useRouter().push('/')
   return null
 }
