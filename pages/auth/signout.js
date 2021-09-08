050e525d946509784cad8cd918f7dbce34463746
diGiovani <alexsandro.dgjr@gmail.com>
2021-09-08 11:23:14 -0300

    just making it work again

--- pages/auth/signout.js
+++ pages/auth/signout.js
@@ -1,11 +1,10 @@
 import { signOut } from 'next-auth/client'
-import { useRouter } from 'next/router'
 
 
 export default function Signout() {
   if (typeof window !== 'undefined') {
     signOut({ callbackUrl: process.env.NEXTAUTH_URL })
   }
-  useRouter().push('/')
+  
   return null
 }
