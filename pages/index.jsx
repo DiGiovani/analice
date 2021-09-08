888b973a9340d251d90ac2eb9531219a00722868
diGiovani <alexsandro.dgjr@gmail.com>
2021-09-08 11:58:09 -0300

    fix: fixing url to new domain

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,10 +17,10 @@ export default function Home() {
         :
         <>
           <h1 className={styles.signUp}><b>Analice</b></h1>
-          <button onClick={() => router.push('https://analiceunica.b2clogin.com/analiceunica.onmicrosoft.com/B2C_1_signup-analice/oauth2/v2.0/authorize?response_type=code+id_token&response_mode=form_post&scope=offline_access%20openid&redirect_uri=https%3A%2F%2Fanalice.vercel.app%2Fapi%2Fauth%2Fcallback%2Fazureb2c&client_id=72c63f0b-2995-4ab6-849e-be9b4f94c35e')} className={styles.signupButton} type="button">
+          <button onClick={() => router.push('https://analiceunica.b2clogin.com/analiceunica.onmicrosoft.com/B2C_1_signup-analice/oauth2/v2.0/authorize?response_type=code+id_token&response_mode=form_post&scope=offline_access%20openid&redirect_uri=https%3A%2F%2Fanalice.unicainstancia.com.br%2Fapi%2Fauth%2Fcallback%2Fazureb2c&client_id=72c63f0b-2995-4ab6-849e-be9b4f94c35e')} className={styles.signupButton} type="button">
             Cadastre-se
           </button>
-          <p>Já tem cadastro? Faça seu <a href="https://analiceunica.b2clogin.com/analiceunica.onmicrosoft.com/B2C_1_analice/oauth2/v2.0/authorize?response_type=code+id_token&response_mode=form_post&scope=offline_access%20openid&redirect_uri=https%3A%2F%2Fanalice.vercel.app%2Fapi%2Fauth%2Fcallback%2Fazureb2c&client_id=72c63f0b-2995-4ab6-849e-be9b4f94c35e">login</a>!</p>
+          <p>Já tem cadastro? Faça seu <a href="https://analiceunica.b2clogin.com/analiceunica.onmicrosoft.com/B2C_1_analice/oauth2/v2.0/authorize?response_type=code+id_token&response_mode=form_post&scope=offline_access%20openid&redirect_uri=https%3A%2F%2Fanalice.unicainstancia.com.br%2Fapi%2Fauth%2Fcallback%2Fazureb2c&client_id=72c63f0b-2995-4ab6-849e-be9b4f94c35e">login</a>!</p>
         </>
         
       }
